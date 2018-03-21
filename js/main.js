2a055a571832462a2ffc10c03152183ab7a22c6d
marcinpiatek <marcin_piatek111@interia.pl>
2018-03-21 15:35:41 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $('button').click(function (e) {
+        e.preventDefault();
+
+
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php',
+            function (data) {
+
+
+                $('#dane-programisty').append('<p>Imię: ' + data.imie + '</p>');
+                $('#dane-programisty').append('<p>Nazwisko: ' + data.nazwisko + '</p>');
+                $('#dane-programisty').append('<p>Zawód: ' + data.zawod + '</p>');
+                $('#dane-programisty').append('<p>Firma: ' + data.firma + '</p>');
+            });
+
+    });
+
+});
